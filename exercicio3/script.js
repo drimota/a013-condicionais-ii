e555b7eb7026df6ba73ea187082e67ff61f37128
drimota <driismota@hotmail.com>
2022-08-08 20:33:36 -0400

    atividade 3 finalizada

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,21 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+const nome = prompt("digite seu nome aqui")
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+let idade = prompt("qual sua idade?")
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
+
+console.log(nome? "seu nome é" + nome :"digite seu nome")
+
+console.log(idade? "sua idade é" + idade:"digite sua idade")
