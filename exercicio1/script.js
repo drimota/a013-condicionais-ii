2c8ce08dc9bc667d36cb461895585b5a419e484e
drimota <driismota@hotmail.com>
2022-08-08 20:22:42 -0400

    atividade 1 finalizada

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,24 @@
-const num = //seu numero aqui
+const numero = Number(prompt("Digite um número"))
+
+// Ifs Aninhados
+
+// if(numero % 2===0){
+//     console.log("é divisivel por 2")
+//     if(numero % 3===0){
+//         console.log("É divisível por 3")
+//     } else {
+//         console.log("Não é divisível por 3")
+//     } 
+// } else {
+//     console.log("não divisivel por 2")
+// }
+
+// IF com operador lógico
+
+if(numero % 2===0 || numero % 3===0) {
+    console.log("é divisivel")
+} else {
+    console.log("não é divisivel")
+}
+
 
